9b74e8d8ee1961b6e668712eff6904fd048b3e5a
parnparnn <64050129@kmitl.ac.th>
2023-04-22 23:08:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loginactivity/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.loginactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+
+
+import java.io.IOException;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button login = findViewById(R.id.login);
+        EditText usernameText = findViewById(R.id.username);
+        EditText passwordText = findViewById(R.id.password);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameText.getText().toString();
+                String password = passwordText.getText().toString();
+
+                    if (username.equals("") || password.equals("")){
+                        Log.e("MainActivity","Please Enter Username and Password");
+                    }
+                    else {
+                        Log.d("MainActivity","username = " + username + " password = " + password);
+                        try {
+                            run();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+
+            }
+
+        });
+
+    }
+    void run() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://publicobject.com/helloworld.txt")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("MainActivity",myResponse);
+
+                    }
+                });
+            }
+        });
+    }
+
+}
